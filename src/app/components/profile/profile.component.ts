7443899b07b177875aa9fc9d42839b4cd70bc43c
jc-hsiao <marshfire@live.com>
2020-04-20 23:01:34 -0400

    styled welcome page and fix some load problem

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService} from 'src/app/services/user.service';
 import { User } from 'src/app/models/user';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -11,14 +12,16 @@ export class ProfileComponent implements OnInit {
 
   currentUser: User = new User();
   constructor(
+    private route: ActivatedRoute,
     private userService: UserService
   ) { }
 
   ngOnInit() {
-    this.userService.getUser().subscribe( u => {
-      this.currentUser = u;
+    this.route.params.subscribe(params => {
+      this.userService.getUser().subscribe( u => {
+        this.currentUser = u;
+      }) 
     })
-    
   }
 
 }
